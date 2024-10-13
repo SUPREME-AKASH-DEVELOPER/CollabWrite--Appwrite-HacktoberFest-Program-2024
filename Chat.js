01a48431d83e2c960b4d19afbbfb4a01a224406c
Akash Lakhwan <142366831+SUPREME-AKASH-DEVELOPER@users.noreply.github.com>
2024-10-13 18:54:11 +0530

    Create Chat.js

---
+++ Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      <h2>Team Chat</h2>
+      <p>Chat functionality coming soon!</p>
+    </div>
+  );
+};
+
+export default Chat;
